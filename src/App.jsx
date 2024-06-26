6b19e6dc470b06ea6bf6a1739781cb2832e3cd0c
HanyMohamed <hsaad9231@gmail.com>
2024-06-26 11:12:22 +0300

    start Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+
+function App() {
+
+  return (
+    <>
+      <Typography variant='h2'>
+        Hello There welcome to my project
+      </Typography>
+      <h1> hany moahemd </h1>
+    </>
+  )
+}
+
+export default App
